d88262174ee99c9d8d4262473fa0a5547859e792
Riko <eldoradozolotoe100@mail.ru>
2023-07-19 16:27:52 +0800

    Add files

---
+++ myShop/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_page, name='main'),
+    path('about', views.about, name='about'),
+    path('sales', views.sales, name='sales'),
+    path('catalog', views.catalog, name='catalog'),
+    path('faq', views.faq, name='faq'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+]
---
+++ myShop/main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def main_page(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def sales(request):
+    return render(request, 'main/sales.html')
+
+
+def catalog(request):
+    return render(request, 'main/catalog.html')
+
+
+def faq(request):
+    return render(request, 'main/faq.html')
+
+
+def login(request):
+    return render(request, 'main/login.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
